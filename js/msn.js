6cf41167bbb33067f93be581b18a531736a1af1b
Turbobent <kasp728h@edu.mercantec.dk>
2024-11-26 12:21:49 +0100

    btn ui upd

--- js/msn.js
+++ js/msn.js
@@ -1,6 +1,6 @@
 // Change the username
 function changeUsername() {
-    const newUsername = prompt('Enter a new username:');
+    const newUsername = prompt('skriv dit navn:');
     if (newUsername) {
         const usernameDisplay = document.querySelector('.chat-header p:first-child');
         usernameDisplay.textContent = `Username: ${newUsername}`;
